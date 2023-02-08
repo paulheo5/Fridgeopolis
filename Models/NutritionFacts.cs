5d201e7fb0ff3f93c7b4965bf2459fd053fcbddf
CBush88 <christopherbush88@gmail.com>
2023-02-08 14:42:51 -0500

    Model for nutrition tracking

---
+++ Models/NutritionFacts.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Permissions;
+
+namespace Fridgeopolis.Models
+{
+    public class NutritionFacts
+    {
+        public int NutritionId { get; set; }
+        public string FoodName { get; set; }
+        public int Calories { get; set; }
+        public int Carbohydrates { get; set; }
+        public int Protein { get; set; }
+        public int Fat { get; set; }
+        public int Phosphorus { get; set; }
+        public int Potassium { get; set; }
+        public int Sodium { get; set; }
+    }
+}
