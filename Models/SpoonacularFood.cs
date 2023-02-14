965dbd5b962e8380a8db1f8b9cde5e1567567d12
CBush88 <christopherbush88@gmail.com>
2023-02-14 16:13:15 -0500

    Updated SpoonacularFood model

--- Models/SpoonacularFood.cs
+++ Models/SpoonacularFood.cs
@@ -8,12 +8,50 @@
         public double percentOfDailyNeeds { get; set; }
     }
 
+    public class CaloricBreakdown
+    {
+        public double percentProtein { get; set; }
+        public double percentFat { get; set; }
+        public double percentCarbs { get; set; }
+    }
+
+    public class Flavonoid
+    {
+        public string name { get; set; }
+        public double amount { get; set; }
+        public string unit { get; set; }
+    }
+
     public class Good
     {
         public string title { get; set; }
         public string amount { get; set; }
         public bool indented { get; set; }
         public double percentOfDailyNeeds { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double amount { get; set; }
+        public string unit { get; set; }
+        public List<Nutrient> nutrients { get; set; }
+    }
+
+    public class Nutrient
+    {
+        public string name { get; set; }
+        public double amount { get; set; }
+        public string unit { get; set; }
+        public double percentOfDailyNeeds { get; set; }
+    }
+
+    public class Property
+    {
+        public string name { get; set; }
+        public double amount { get; set; }
+        public string unit { get; set; }
     }
 
     public class SpoonacularFood
@@ -24,7 +62,19 @@
         public string protein { get; set; }
         public List<Bad> bad { get; set; }
         public List<Good> good { get; set; }
-        public long expires { get; set; }
-        public bool isStale { get; set; }
+        public List<Nutrient>? nutrients { get; set; }
+        public List<Property>? properties { get; set; }
+        public List<Flavonoid>? flavonoids { get; set; }
+        public List<Ingredient>? ingredients { get; set; }
+        public CaloricBreakdown? caloricBreakdown { get; set; }
+        public WeightPerServing? weightPerServing { get; set; }
+        public long? expires { get; set; }
+        public bool? isStale { get; set; }
+    }
+
+    public class WeightPerServing
+    {
+        public int amount { get; set; }
+        public string unit { get; set; }
     }
 }
