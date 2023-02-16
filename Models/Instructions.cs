8f85f25e24b009a5f3760613e0632519d26349b7
Paul Heo <pheo@macalester.edu>
2023-02-16 14:53:24 -0600

    Put instructions in the same recipe view

--- Models/Instructions.cs
+++ Models/Instructions.cs
@@ -4,20 +4,20 @@ using Newtonsoft.Json;
 namespace Fridgeopolis.Models
 {
 
-    public class Instructions
-    {
-        public string name { get; set; }
-        public List<Step> steps { get; set; }
-    }
+    //public class Instructions
+    //{
+    //    public string name { get; set; }
+    //    public List<Step> steps { get; set; }
+    //}
 
-    public class Step
-    {
+    //public class Step
+    //{
 
-        public string step { get; set; }
+    //    public string step { get; set; }
  
-    }
+    //}
 
-    public class Temperature
+    public class Instructions
     {
         public double number { get; set; }
         public string unit { get; set; }
