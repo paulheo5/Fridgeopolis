108b50e0035461cb56ea4e234402a1801eb81e8a
Paul Heo <pheo@macalester.edu>
2023-02-17 14:20:00 -0600

    fixing small changes in create view

--- Controllers/PropertyModelsController.cs
+++ Controllers/PropertyModelsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Renipe.DataContext;
+using Renipe.Models;
 
 namespace Renipe.Controllers
 {
